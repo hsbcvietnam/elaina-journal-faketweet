cf2cc0837735a254f0226c6489f246d9554c7991
hsbcvietnam <hsbcvietnam.bank@gmail.com>
2021-11-25 11:06:07 +0700

    test

--- routes/index.js
+++ routes/index.js
@@ -9,20 +9,11 @@ const router = express.Router();
 const folderName = __dirname.split(path.sep).pop();
 // var folderPath = __dirname + `../../../projects/${folderName}/`;
 
-router.get('/en', function(req, res, next) {
-  res.render(`projects/index_en`, {
+router.get('/tweet', function(req, res, next) {
+  res.render(`projects/index`, {
     folderName: '',
     csrfToken: req.csrfToken(),
-    language: 'english',
-    campaign: 'promotion',
-  });
-});
-
-router.get('/vn', function(req, res, next) {
-  res.render(`projects/index_vn`, {
-    folderName: '',
-    csrfToken: req.csrfToken(),
-    language: 'vietnamese',
+    language: 'engish',
     campaign: 'promotion',
   });
 });
